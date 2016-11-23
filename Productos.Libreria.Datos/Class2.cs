7ff2def91837955494126244fbe65914776830ef
deyvis <deyvis_olcc@hotmail.com>
2016-11-23 18:34:29 -0500

    ms2

---
+++ Productos.Libreria.Datos/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Productos.Libreria.Datos
+{
+    class Class2
+    {
+        //git222
+    }
+}
